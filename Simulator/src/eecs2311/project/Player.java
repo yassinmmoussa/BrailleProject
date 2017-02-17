2173dca33c8cf67fb8ab984327d42f28af6917b1
deruga04 <derekli.3628@gmail.com>
2017-02-17 11:18:45 -0500

    Added capability for text file to speech

--- Simulator/src/eecs2311/project/Player.java
+++ Simulator/src/eecs2311/project/Player.java
@@ -1,5 +1,10 @@
 package eecs2311.project;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.logging.Logger;
+
 import com.sun.speech.freetts.Voice;
 import com.sun.speech.freetts.VoiceManager;
 
@@ -10,7 +15,11 @@ public class Player {
 		
 	}
 	
-	public void freeTTS(String text)
+	/**
+	 * Outputs sound based on the input string parameter.
+	 * @param text - The text to be converted to audio.
+	 */
+	public void textToSpeech(String text)
 	{
 		String VOICENAME_kevin = "kevin";
 		Voice voice;
@@ -18,5 +27,32 @@ public class Player {
 		voice = vm.getVoice(VOICENAME_kevin);
 		voice.allocate();
 		voice.speak(text);
+	}
+	
+	public void textFileToSpeech(String fileName) throws Exception
+	{
+		try
+		{
+			Logger.getGlobal().info("Opening file");
+			
+			File file = new File(fileName);
+			
+			Logger.getGlobal().info(""+file.exists());
+		
+			Scanner s = new Scanner(new File(fileName));
+			
+			Logger.getGlobal().info("Read file");
+			
+			while (s.hasNextLine())
+			{
+				textToSpeech(s.nextLine());
+			}
+			s.close();
+		}
+		catch (IOException e)
+		{
+			throw new IOException("File can not be opened.");
+		}
+		
 	}
 }
