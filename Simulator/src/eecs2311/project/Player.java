fecd3255b7ba9da57941c1d1ca13936e31ab3fd6
yassinmmoussa <yassinmmoussa@yahoo.com>
2017-02-25 20:32:59 -0500

    Wrote the constructor for the player app, take a look at it
    You'll understand what I did if you read the code it's pretty
    straightforward. The most important thing I added is the switch-case,
    because that's what's going to be handling the different things the
    player app should be able to do.
    Every case corresponds to a thing the player app should handle, the
    names of the methods should make it pretty obv which cases correspond to
    what.
    The methods take the scanner as imput because the tags (<a>, <t> etc.)
    just indicate the BEGINING of the block of whatever.
    So in out format every block will have to be terminated by another tag,
    let's say <end>, and so since the methods take the scanner as input,
    they will keep reading the next lines and doing what they do until the
    next line is <end> at which point they just return and the main while
    loop in the constructor keeps going.

--- Simulator/src/eecs2311/project/Player.java
+++ Simulator/src/eecs2311/project/Player.java
@@ -1,6 +1,7 @@
 package eecs2311.project;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Scanner;
 import java.util.logging.Logger;
@@ -9,18 +10,49 @@ import com.sun.speech.freetts.Voice;
 import com.sun.speech.freetts.VoiceManager;
 
 public class Player {
-	
-	public Player()
-	{
-		
+
+	private Simulator simulator;
+	private int buttonNumber;
+	private int cellNumber;
+
+	public Player(File inputFile) throws FileNotFoundException {
+
+		String nextLine;
+		Scanner scanner = new Scanner(inputFile);
+
+		this.buttonNumber = scanner.nextInt();
+		this.cellNumber = scanner.nextInt();
+		simulator = new Simulator(cellNumber, buttonNumber);
+
+		while (scanner.hasNextLine()) {
+
+			nextLine = scanner.nextLine();
+			switch (nextLine) {
+			case "<t>":
+				Player.readText(scanner);
+				break;
+			case "<a>":
+				Player.playAudio(scanner);
+				break;
+			case "<c>":
+				Player.processAction(scanner);
+				break;
+			default:
+				break;
+
+			}
+
+		}
+		scanner.close();
 	}
-	
+
 	/**
 	 * Outputs sound based on the input string parameter.
-	 * @param text - The text to be converted to audio.
+	 * 
+	 * @param text
+	 *            - The text to be converted to audio.
 	 */
-	public void textToSpeech(String text)
-	{
+	private void tts(String text) {
 		String VOICENAME_kevin = "kevin";
 		Voice voice;
 		VoiceManager vm = VoiceManager.getInstance();
@@ -28,25 +60,32 @@ public class Player {
 		voice.allocate();
 		voice.speak(text);
 	}
-	
-	public void textFileToSpeech(String fileName) throws Exception
-	{
-		try
-		{
+
+	public void ttsFile(String fileName) throws Exception {
+		try {
 			File file = new File(fileName);
 			Scanner s = new Scanner(file);
-			
-			while (s.hasNextLine())
-			{
-				textToSpeech(s.nextLine());
+
+			while (s.hasNextLine()) {
+				tts(s.nextLine());
 			}
 			s.close();
-		}
-		catch (IOException e)
-		{
+		} catch (IOException e) {
 			throw new IOException("File can not be opened.");
 		}
-		
-	}
-}
 
+	}
+
+	private static void playAudio(Scanner scanner) {
+
+	}
+
+	private static void processAction(Scanner scanner) {
+
+	}
+
+	private static void readText(Scanner scanner) {
+
+	}
+
+}
