4775b915ab4b8889c4e302492167441d6a5dcf66
Qassim <allauddq@gmail.com>
2017-03-29 21:53:15 -0400

    Added Record Button inside GUI. Also Added Browse Button that returns the filepath of the selected file in string format. IMPORTANT: You will need to Add "SwingSoundRecorder2" into external JAR file inorder for record button to work.

--- Simulator/src/eecs2311/project/DummyGraph.java
+++ Simulator/src/eecs2311/project/DummyGraph.java
@@ -1,5 +1,8 @@
 package eecs2311.project;
 
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -11,22 +14,49 @@ import com.mxgraph.swing.mxGraphComponent;
 import com.mxgraph.util.mxRectangle;
 import com.mxgraph.view.mxGraph;
 
+import net.codejava.sound.SwingSoundRecorder;
+
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Component;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class DummyGraph {
 
+	private static String filepath = "";
+
+	private static String browseFile() {
+		JFileChooser chooser = new JFileChooser();
+		chooser.setCurrentDirectory(new java.io.File("."));
+		chooser.setDialogTitle("choosertitle");
+		chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+		chooser.setAcceptAllFileFilterUsed(false);
+
+		if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+			// System.out.println("getCurrentDirectory(): " +
+			// chooser.getCurrentDirectory());
+			System.out.println("getSelectedFile() : " + chooser.getSelectedFile().getName());
+		} else {
+			System.out.println("No Selection ");
+		}
+		return chooser.getSelectedFile().getAbsolutePath();
+	}
+
 	public static void main(String args[]) {
+
 		JFrame frame = new JFrame();
 		frame.setSize(600, 600);
 		frame.getContentPane().setLayout(null);
 
+		JPanel mainPanel = new JPanel();
+		mainPanel.setBounds(0, 0, frame.getWidth(), frame.getHeight());
+		mainPanel.setLayout(null);
+
 		JPanel panel = new JPanel();
-		panel.setBounds(70, 28, 134, 800);
+		panel.setBounds(50, 50, 135, 800);
 		panel.setLayout(new BorderLayout());
-		frame.getContentPane().add(panel);
-		frame.setVisible(true);
 
 		mxGraph graph = new mxGraph();
 
@@ -47,7 +77,6 @@ public class DummyGraph {
 			Object e2 = graph.insertEdge(parent, null, null, v2, v3);
 			Object e3 = graph.insertEdge(parent, null, null, v3, v4);
 			Object e4 = graph.insertEdge(parent, null, null, v3, v5);
-			
 
 			layout.execute(graph.getDefaultParent());
 		} finally {
@@ -56,8 +85,38 @@ public class DummyGraph {
 		mxGraphComponent idk = new mxGraphComponent(graph);
 		panel.revalidate();
 		panel.add(idk);
-		frame.repaint();
-		
+
+		JPanel btnPanel = new JPanel();
+		btnPanel.setBounds(200, 50, 400, 50);
+		btnPanel.setLayout(new BorderLayout());
+
+		// add record buttons as panel
+		SwingSoundRecorder recordBtn = new SwingSoundRecorder();
+		recordBtn.setVisible(true);
+		btnPanel.add(recordBtn);
+
+		/*
+		 * Browse button will open FileChooser and when you select file name, it
+		 * will return the filepath of the selected file
+		 */
+		JButton browse = new JButton("Browse..");
+		browse.setBounds(235, 100, 100, 30);
+		browse.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// open file chooser and select file path
+				filepath = browseFile();
+				System.out.println("filepath : " + filepath);
+
+			}
+		});
+
+		// add the two panels to the frame
+		mainPanel.add(panel);
+		mainPanel.add(btnPanel);
+		mainPanel.add(browse);
+		frame.getContentPane().add(mainPanel);
+		frame.setVisible(true);
 
 	}
+
 }
