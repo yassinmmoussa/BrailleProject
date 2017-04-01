8e3d4fe1ff3a17682acf36305f5879e23a2d58cc
Qassim <allauddq@gmail.com>
2017-04-01 17:34:20 -0400

    Added Save button.
    You can save a file and it will only save in .txt format so no need to
    add the extension manually when saving.
    Saving will place a string into a .txt file you just saved.

--- Simulator/src/eecs2311/project/DummyGraph.java
+++ Simulator/src/eecs2311/project/DummyGraph.java
@@ -1,11 +1,5 @@
 package eecs2311.project;
 
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
-import javax.swing.JFileChooser;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-
 import com.mxgraph.layout.mxCompactTreeLayout;
 import com.mxgraph.layout.mxGraphLayout;
 import com.mxgraph.layout.hierarchical.mxHierarchicalLayout;
@@ -22,15 +16,29 @@ import java.awt.Component;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.filechooser.*;
+import javax.swing.filechooser.FileFilter;
 
 public class DummyGraph {
 
 	private static String filepath = "";
+	private static File lastpath;
+	private static JFileChooser chooser = new JFileChooser();
+	private static FileNameExtensionFilter filter = new FileNameExtensionFilter(".txt", "txt");
+
+	
 
 	private static String browseFile() {
-		JFileChooser chooser = new JFileChooser();
-		chooser.setCurrentDirectory(new java.io.File("."));
-		chooser.setDialogTitle("choosertitle");
+		if (lastpath != null) {
+			chooser.setCurrentDirectory(lastpath);
+		} else {
+			chooser.setCurrentDirectory(new File(System.getProperty("user.home")));
+		}
+		chooser.setDialogTitle("Browse for file...");
 		chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
 		chooser.setAcceptAllFileFilterUsed(false);
 
@@ -38,14 +46,32 @@ public class DummyGraph {
 			// System.out.println("getCurrentDirectory(): " +
 			// chooser.getCurrentDirectory());
 			System.out.println("getSelectedFile() : " + chooser.getSelectedFile().getName());
+			lastpath = chooser.getSelectedFile().getParentFile();
+			return chooser.getSelectedFile().getAbsolutePath();
+
 		} else {
-			System.out.println("No Selection ");
+			return "";
 		}
-		return chooser.getSelectedFile().getAbsolutePath();
+	}
+
+	public static void saveFile(String scenario) {
+		chooser.setFileFilter(filter);
+		if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+			try (FileWriter fw = new FileWriter(chooser.getSelectedFile()+".txt")) {
+				fw.write(scenario.toString());
+				JOptionPane.showMessageDialog(chooser, "File saved successfully.");
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+			}
+			lastpath = chooser.getSelectedFile().getParentFile();
+		}else{
+			return;
+		}
 	}
 
 	public static void main(String args[]) {
 
+		String scenario = "This is where the scenario format will be appended";
 		JFrame frame = new JFrame();
 		frame.setSize(600, 600);
 		frame.getContentPane().setLayout(null);
@@ -110,10 +136,25 @@ public class DummyGraph {
 			}
 		});
 
+		/*
+		 * Save button will open FileChooser and allow user to save their
+		 * scenario format into a .txt file
+		 * 
+		 */
+		JButton save = new JButton("Save");
+		save.setBounds(235, 150, 100, 30);
+		save.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// open file chooser and select file path
+				saveFile(scenario);
+			}
+		});
+
 		// add the two panels to the frame
 		mainPanel.add(panel);
 		mainPanel.add(btnPanel);
 		mainPanel.add(browse);
+		mainPanel.add(save);
 		frame.getContentPane().add(mainPanel);
 		frame.setVisible(true);
 
